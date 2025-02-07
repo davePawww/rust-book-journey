c712e44001414efdcdfd6fc34c01810409621c6e
DavePaurillo <paurillo.dave@gmail.com>
2025-02-07 14:06:34 +0800

    hello world

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,11 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+// Compiling the program
+// rustc main.rs
 
-// rustc main.rs - to compile the program.
-// ./main - to run the program.
+// Running the program
+// ./main
 
 // main is the entry point of the program - it is a special function: it is the always first code that runs in every executable Rust program.
 
 // rust uses 4 spaces to indent code.
 
+// adding ! in println makes it a Rust macro.
+// without ! it would be a function.               -- what is the difference between a macro and a function?
