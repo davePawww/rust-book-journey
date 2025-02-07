643034a2c9e42cf52a2cae25cd3ac741566618fd
DavePaurillo <paurillo.dave@gmail.com>
2025-02-07 14:06:12 +0800

    hello world

---
+++ hello-world/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// rustc main.rs - to compile the program.
+// ./main - to run the program.
+
+// main is the entry point of the program - it is a special function: it is the always first code that runs in every executable Rust program.
+
+// rust uses 4 spaces to indent code.
+
