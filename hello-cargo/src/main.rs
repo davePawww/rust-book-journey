01ba580a9599fbda1494d9563a5b9e1aa480e922
DavePaurillo <paurillo.dave@gmail.com>
2025-02-07 14:36:13 +0800

    hello-cargo

---
+++ hello-cargo/src/main.rs
@@ -0,0 +1,19 @@
+// cargo is Rust's build system and package manager.
+// cargo new <project-name> - to create a new cargo project.
+// cargo build - to compile the program.
+// cargo run - to compile and run the program.
+// cargo check - to check the program without compiling it.
+// cargo build --release - to compile the program in release mode.
+
+// TOML (Tom’s Obvious, Minimal Language) is Cargo's configuration format.
+// [package] is the root of the configuration.
+// name is the name of the package.
+// version is the version of the package.
+// edition is the edition of the Rust language.
+
+// [dependencies] is the section for listing dependencies.
+// In Rust, packages are called crates.
+
+fn main() {
+    println!("Hello, world!");
+}
